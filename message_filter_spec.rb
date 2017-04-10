b4d7d4d37ae4c85144cb1efd2d63d056eef7ce42
ikemura <kazutaka.ikemura.024@ctc-g.co.jp>
2017-04-10 14:06:34 +0900

    Add first spec

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -1,3 +1,7 @@
 require_relative 'message_filter'
 describe MessageFilter do
+  it 'detects message with NG world' do
+    filter = MessageFilter.new('foo')
+    expect(filter.detect?('hello from foo')).to eq true
+  end
 end
