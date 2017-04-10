408737f4e0e5480c016da95924175d82c3195107
ikemura <kazutaka.ikemura.024@ctc-g.co.jp>
2017-04-10 14:51:22 +0900

    Omit spec descriptions

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -4,10 +4,10 @@ describe MessageFilter do
   before do
     @filter = MessageFilter.new('foo')
   end
-  it 'detects message with NG world' do
+  it {
     expect(@filter).to be_detect('hello from foo')
-  end
-  it 'dose not detect message without NG word' do
+  }
+  it {
     expect(@filter).not_to be_detect('hello, world!')
-  end
+  }
 end
